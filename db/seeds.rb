8562e6dec6b1967fc5b814744c41f77693ad8c07
Max Dolgih <maxim.dolgih@gmail.com>
2018-04-10 00:57:17 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#patient.rb
+Patient.destroy_all
+100.times do
+  Patient.create(first_name: Forgery('name').first_name, last_name: Forgery('name').last_name)
+end
+
+#hospital.rb
+Hospital.destroy_all
+100.times do
+  Hospital.create(name: "#{Forgery('name').full_name}. Hospital", city: Forgery('address').city)
+end
+
+#polyclinic.rb
+Polyclinic.destroy_all
+50.times do
+  Polyclinic.create(name: "#{Forgery('name').full_name}. Hospital", city: Forgery('address').city)
+end
+Hospital.pluck(:id).shuffle.each do |hospital_id|
+  Polyclinic.create(hospital_id: hospital_id, name: "#{Forgery('name').full_name}. Clinic")
+end
+
+# employee_category.rb
+EmployeeCategory.create
+#employee.rb
+# Employee.create(first_name: Forgery('name').first_name, last_name: Forgery('name').last_name)
+
+
+# patient_card.rb
+# analyze.rb
+# bed.rb
+# block.rb
+# contract.rb
+# department.rb
+# department_type.rb
+# employee.rb
+# hospital_staff.rb
+# laboratory.rb
+# polyclinic.rb
+# polyclinic_staff.rb
+# ward.rb
