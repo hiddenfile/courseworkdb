e44e91373b8823c201725386f0e066f545dcdaa5
Max Dolgih <maxim.dolgih@gmail.com>
2018-05-14 23:53:40 +0300

    fix validation

--- app/models/patient_card.rb
+++ app/models/patient_card.rb
@@ -18,8 +18,8 @@
 
 class PatientCard < ApplicationRecord
   belongs_to :clinic, polymorphic: true
-  belongs_to :hospital, foreign_key: :clinic_id, foreign_type: 'Hospital'
-  belongs_to :polyclinic, foreign_key: :clinic_id, foreign_type: 'Polyclinic'
-  belongs_to :employee
+  belongs_to :hospital, foreign_key: :clinic_id, foreign_type: 'Hospital', required: false
+  belongs_to :polyclinic, foreign_key: :clinic_id, foreign_type: 'Polyclinic', required: false
   belongs_to :patient
+  belongs_to :employee
 end
